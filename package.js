830e5e73322de1994617b3ccdb013bf22ceebfb9
David Burles <dburles@gmail.com>
2014-05-14 15:09:42 +1000

    Update package.js

--- package.js
+++ package.js
@@ -7,8 +7,6 @@ Package.on_use(function (api) {
   api.add_files('lib/client.js', 'client');
   api.add_files('lib/server.js', 'server');
 
-  if (typeof api.export !== 'undefined') {
-    api.export('Presences', ['client', 'server']);
-    api.export('Presence', ['client', 'server']);
-  }
+  api.export('Presences', ['client', 'server']);
+  api.export('Presence', ['client', 'server']);
 });
