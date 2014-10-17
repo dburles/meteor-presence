6e435681ba4d136d5b9efda3bf1f6426c92d4c51
David Burles <dburles@gmail.com>
2014-10-18 09:09:02 +1100

    updated for 0.9.0+

--- package.js
+++ package.js
@@ -1,8 +1,11 @@
 Package.describe({
-  summary: "A package to help track users' presence"
+  name: "tmeasday:presence",
+  summary: "A package to help track users' presence",
+  version: "1.0.0",
+  git: "https://github.com/dburles/meteor-presence.git"
 });
 
-Package.on_use(function (api) {
+Package.onUse(function (api) {
   api.add_files('lib/common.js', ['client', 'server']);
   api.add_files('lib/client.js', 'client');
   api.add_files('lib/server.js', 'server');
