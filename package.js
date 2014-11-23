20ea6f055dbc333a56ffae743652dfdfaf906ba9
Ratnadeep Debnath <rtnpro@gmail.com>
2014-11-23 18:19:12 +0530

    Bump to Meteor 1.0.

--- package.js
+++ package.js
@@ -1,14 +1,15 @@
 Package.describe({
   name: "tmeasday:presence",
   summary: "A package to help track users' presence",
-  version: "1.0.1",
+  version: "1.0.2",
   git: "https://github.com/dburles/meteor-presence.git"
 });
 
 Package.onUse(function (api) {
-  api.add_files('lib/common.js', ['client', 'server']);
-  api.add_files('lib/client.js', 'client');
-  api.add_files('lib/server.js', 'server');
+  api.versionsFrom('1.0');
+  api.addFiles('lib/common.js', ['client', 'server']);
+  api.addFiles('lib/client.js', 'client');
+  api.addFiles('lib/server.js', 'server');
 
   api.export('Presences', ['client', 'server']);
   api.export('Presence', ['client', 'server']);
