b1ac31d374d36da9fcf1453418c843184f16a700
David Burles <dburles@gmail.com>
2015-03-05 21:07:14 +1100

    version bump + versions.json => .versions

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "tmeasday:presence",
   summary: "A package to help track users' presence",
-  version: "1.0.3",
+  version: "1.0.4",
   git: "https://github.com/dburles/meteor-presence.git"
 });
 
