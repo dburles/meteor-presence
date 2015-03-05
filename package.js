b7c3b9b7a0cd6931aac17103effdb7807b0ef0ca
David Burles <dburles@gmail.com>
2015-03-05 21:58:04 +1100

    use mongo

--- package.js
+++ package.js
@@ -1,17 +1,19 @@
 Package.describe({
   name: "tmeasday:presence",
   summary: "A package to help track users' presence",
-  version: "1.0.4",
+  version: "1.0.5",
   git: "https://github.com/dburles/meteor-presence.git"
 });
 
 Package.onUse(function (api) {
   api.versionsFrom('1.0');
   api.use('tracker', 'client');
-  api.addFiles('lib/common.js', ['client', 'server']);
+  api.use('mongo');
+
+  api.addFiles('lib/common.js');
   api.addFiles('lib/client.js', 'client');
   api.addFiles('lib/server.js', 'server');
 
-  api.export('Presences', ['client', 'server']);
-  api.export('Presence', ['client', 'server']);
+  api.export('Presences');
+  api.export('Presence');
 });
